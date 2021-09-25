9d78c1635f197e95154fa21154ca014f961487e2
Samuel Nunoo <38366722+samuelnunoo@users.noreply.github.com>
2021-09-25 12:15:14 -0700

    I wanna cry

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - PersonalWebsite',
-    title: 'PersonalWebsite',
+    title: 'Portfolio',
     htmlAttrs: {
       lang: 'en'
     },
